6fd8ea51112a35723c01db17226c55eaa9f0d51e
haticenurolgun <hatiicenurolgun@gmail.com>
2025-08-29 21:57:41 +0300

    Initial commit

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data=[
+    {"text": "Bir ahtapotun üç tane kalbi vardır.", "answer": "d"},
+    {"text": "Çin Seddi, Ay'dan çıplak gözle görülebilir.", "answer": "y"},
+    {"text": "İnsan DNA'sı ile bir muzun DNA'sı %50 oranında benzerdir.", "answer": "d"},
+    {"text": "Japon balıklarının hafızası sadece üç saniyedir.", "answer": "y"},
+    {"text": "Gözleriniz açıkken hapşırmanız fiziksel olarak imkansızdır.", "answer": "d"},
+    {"text": "İskoçya'nın ulusal hayvanı tek boynuzlu attır (unicorn).", "answer": "d"},
+    {"text": "Bir şimşeğin sıcaklığı, Güneş'in yüzey sıcaklığından daha fazladır.", "answer": "d"},
+    {"text": "Venüs, Güneş'e Merkür'den daha uzak olmasına rağmen en sıcak gezegendir.", "answer": "d"},
+    {"text": "Timsahlar dillerini dışarı çıkaramazlar.", "answer": "d"},
+    {"text": "Ampulü Thomas Edison icat etmiştir.", "answer": "y"},
+    {"text": "Kahve, aslında bir meyvenin çekirdeğidir.", "answer": "d"},
+    {"text": "Dünyanın en küçük ülkesi Monako'dur.", "answer": "y"}
+]
---
+++ main.py
@@ -0,0 +1,23 @@
+from questions import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank=[]
+
+for question in question_data:
+    question_text=question["text"]
+    question_answer=question["answer"]
+    new_question=Question(question_text,question_answer)
+    question_bank.append(new_question)
+
+quiz= QuizBrain(question_bank)
+
+while quiz.still_any_question():
+    quiz.next_question()
+
+print("Tebrikler,Quizi tamamladınız.")
+print(f"Toplam skorunuz: {quiz.score}/{quiz.question_number}")
+
+
+
+
